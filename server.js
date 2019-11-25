e19589e56e476263dbdb92d57f353c5b405d1d0d
Neely Anne <neely.anne@yahoo.com>
2019-11-25 12:11:35 -0500

    not functioning

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ const express = require('express');
 const app = express();
 //allows it to be run on a locla server or on heroku
 const PORT = process.env.PORT || 3000;
-
+//configure middleware
 app.use(express.static(path.join(__dirname, "./app/public")));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
+//require our routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 app.listen(PORT, function() {
   console.log('Listening on PORT: ' + PORT);
